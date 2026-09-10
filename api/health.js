f647bf3fc55f030e23c1c608b12eb7ff8229b2a5
Beto321df <h4ck666q@gmail.com>
2026-09-10 17:46:23 -0500

    Fix health endpoint independent from catch-all backend

--- api/health.js
+++ api/health.js
@@ -1,1 +1,13 @@
-module.exports = require('./[...route].js');
+module.exports = function (req, res) {
+  res.status(200);
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.end(JSON.stringify({
+    ok: true,
+    service: 'znexus-api',
+    version: '2026.9-free',
+    firebaseConfigured: Boolean((process.env.FIREBASE_SERVICE_ACCOUNT_JSONZ || process.env.FIREBASE_SERVICE_ACCOUNT_JSON) && process.env.FIREBASE_DATABASE_URL),
+    linkvertiseConfigured: Boolean(process.env.LINKVERTISE_ANTI_BYPASS_TOKEN)
+  }));
+};
