c9a9683fe4afd2fd0c5a4194f5766d6268e7c906
Beto321df <h4ck666q@gmail.com>
2026-09-11 21:24:12 -0500

    remove health function to stay within Vercel Hobby function limit

--- api/health.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (req, res) => {
-  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
-  res.setHeader('Content-Type', 'application/json; charset=utf-8');
-  res.status(200).json({
-    ok: true,
-    service: 'znexus-api',
-    version: '2026.9-hardened'
-  });
-};
